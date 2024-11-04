bf1ac5499fae385289e189337d82ee81d3b11acc
Felix Zedén Yverås <felix@fzy.se>
2024-11-04 16:51:03 +0100

    feat: configure code style checks
    Using ktfmt and detekt. Also configure commit message verification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.detekt)
 }
 
 android {
@@ -15,39 +19,29 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        vectorDrawables { useSupportLibrary = true }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+    kotlinOptions { jvmTarget = "1.8" }
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
+    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -65,3 +59,12 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
+
+detekt {
+    buildUponDefaultConfig = true
+    config.setFrom("$rootDir/detekt.yml")
+}
+
+tasks.withType<Detekt>().configureEach { jvmTarget = "1.8" }
+
+tasks.withType<DetektCreateBaselineTask>().configureEach { jvmTarget = "1.8" }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,38 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.conventionalCommits)
+    alias(libs.plugins.spotless)
 }
+
+spotless {
+    ratchetFrom("origin/main")
+
+    format("misc") {
+        target("*.md", ".gitignore")
+
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+        encoding("utf-8")
+    }
+
+    kotlin {
+        ktfmt("0.53").kotlinlangStyle().configure {
+            target("**/*.kt")
+            it.setRemoveUnusedImports(true)
+            it.setManageTrailingCommas(true)
+        }
+    }
+}
+
+task("addGitHooks") {
+    group = "verification"
+    copy {
+        from(".hooks/")
+        into(".git/hooks/")
+    }
+}
+
+tasks.getByPath(":app:preBuild").dependsOn(tasks.getByName("addGitHooks"))
